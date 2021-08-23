e9fe3cc9b3c90a27dbb74653e0c2d95cb55176b8
Wojciech Kandziora <wojciech.kandziora@gmail.com>
2021-08-23 22:38:58 +0200

    Add first script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Komputer zagrał ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
