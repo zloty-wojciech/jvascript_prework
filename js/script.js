abef6a82890d90214428100de117f9aee129615b
Wojciech Kandziora <wojciech.kandziora@gmail.com>
2021-08-24 20:20:29 +0200

    Add script.js for computer moves

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier';
+let computerMove = 'nieznany ruch';
 
-printMessage('Komputer zagrał ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+    computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
