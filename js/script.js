05225a45a74d22eaf9c758ff169b2076edc2c502
Wojciech Kandziora <wojciech.kandziora@gmail.com>
2021-08-24 20:27:38 +0200

    Add script.js for player moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Computer
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -8,12 +10,34 @@ if(randomNumber == 1){
     computerMove = 'kamień';
 }
 
-if(randomNumber == 2){
+else if(randomNumber == 2){
     computerMove = 'papier';
 }
 
-if(randomNumber == 3){
+else(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// Player
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+
+else if(playerInput == '2'){
+    playerMove = 'papier';
+}
+
+else(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
